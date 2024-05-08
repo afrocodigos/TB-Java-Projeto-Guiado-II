0c1c7d8edc02796211d431d252c28d50d53347ff
julio-cesar96 <julio_cbs96@hotmail.com>
2024-05-08 19:03:16 -0300

    feat: adicionando metodo de buscar, remover e mostrar todos os funcionarios

--- src/funcionariosStarlabs/ArvoreFuncionarios.java
+++ src/funcionariosStarlabs/ArvoreFuncionarios.java
@@ -24,4 +24,72 @@ public class ArvoreFuncionarios {
     public void inserir(FuncionarioBase funcionario) {
         root = inserirRecursivo(root, funcionario);
     }
+
+    private FuncionarioBase buscarRecursivo(FuncionarioNode node, String nome) {
+        
+        if (node == null || node.funcionario.getNome().equals(nome)) {
+            return node == null ? null : node.funcionario;
+        }
+
+        if (nome.compareTo(node.funcionario.getNome()) < 0) {
+            return buscarRecursivo(node.leftChild, nome);
+        } else {
+            buscarRecursivo(node.rightChild, nome);
+        }
+        
+        return null;
+    }
+
+    public FuncionarioBase buscar(String nome) {
+        return buscarRecursivo(root, nome);
+    }
+
+
+    private FuncionarioNode removerRecursivo(FuncionarioNode node, String nome) {
+        if (node == null) {
+            return null;
+        }
+
+        if (nome.compareTo(node.funcionario.getCargo()) < 0) {
+            node.leftChild = removerRecursivo(node.leftChild, nome);
+        } else if (nome.compareTo(node.funcionario.getNome()) > 0) {
+            node.rightChild = removerRecursivo(node.rightChild, nome);
+        } else {
+            if (node.leftChild == null) {
+                return node.rightChild;
+                } else if (node.rightChild == null) {
+                    return node.leftChild;
+                }
+
+            node.funcionario = minValue(node.rightChild);
+            node.rightChild = removerRecursivo(node.rightChild, node.funcionario.getNome());
+        }
+        return node;
+    }
+
+    public void remover(String nome) {
+        root = removerRecursivo(root, nome);
+    }
+
+    private FuncionarioBase minValue(FuncionarioNode node) {
+        FuncionarioBase menorfuncionario = node.funcionario;
+        while(node.leftChild != null) {
+            menorfuncionario = node.leftChild.funcionario;
+            node = node.leftChild;
+        }
+
+        return menorfuncionario;
+    }
+
+    public void mostarDadosEmOrdem() {
+        mostarDadosEmOrdemRecursivo(root);
+    }
+
+    private void mostarDadosEmOrdemRecursivo(FuncionarioNode node) {
+        if (node != null) {
+            mostarDadosEmOrdemRecursivo(node.leftChild);
+            System.out.println(node.funcionario.getNome() + " - " + node.funcionario.getCargo());
+            mostarDadosEmOrdemRecursivo(node.rightChild);
+        }
+    }
 }
