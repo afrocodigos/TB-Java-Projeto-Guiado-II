b83766fffd34d2f2f10c3a839a838d5af30a2932
Luã Moura <130193631+LhamCode@users.noreply.github.com>
2024-05-11 23:26:37 -0300

    :sparkles: feat: adicionando novos funcionários

---
+++ src/model/Programadores.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Programadores extends FuncionarioBase {
+
+    public Programadores(String nome, String cargo, double salario, String cpf) {
+        super(nome, cargo, salario, cpf);
+
+    }
+
+    public void criar() {
+        System.out.println("CTO criado com sucesso");
+    }
+
+    public void ler() {
+        System.out.println("Nome " + nome + ", Cargo: " + cargo + ", Salário: R$" + salario);
+    }
+
+    public void atualizarDados(double novoSalario) {
+        this.salario = novoSalario;
+        System.out.println("Dados do CTO atualizado com sucesso!");
+    }
+
+    public void remover() {
+        System.out.println("CTO removido com sucesso");
+    }
+}
---
+++ src/model/SuporteTecnico.java
@@ -0,0 +1,25 @@
+package model;
+
+public class SuporteTecnico extends FuncionarioBase {
+
+    public SuporteTecnico(String nome, String cargo, double salario, String cpf) {
+        super(nome, cargo, salario, cpf);
+    }
+
+    public void criar() {
+        System.out.println("CTO criado com sucesso");
+    }
+
+    public void ler() {
+        System.out.println("Nome " + nome + ", Cargo: " + cargo + ", Salário: R$" + salario);
+    }
+
+    public void atualizarDados(double novoSalario) {
+        this.salario = novoSalario;
+        System.out.println("Dados do CTO atualizado com sucesso!");
+    }
+
+    public void remover() {
+        System.out.println("CTO removido com sucesso");
+    }
+}
