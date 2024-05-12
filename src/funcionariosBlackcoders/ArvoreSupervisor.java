c537dbb53d28d33be220e9ea0825aa35d498e746
hugomacielads <hugomacielcesar@gmail.com>
2024-05-12 18:08:26 -0300

    :sparkles: feat: Adicionando cargo Supervisor
    + Adicionando árvore completa de funcionários na Main

---
+++ src/funcionariosBlackcoders/ArvoreSupervisor.java
@@ -0,0 +1,28 @@
+package funcionariosBlackcoders;
+
+public class ArvoreSupervisor extends ArvoreDeFuncionarios<Supervisor>{
+    @Override
+    public void adicionar(Supervisor funcionario) {
+        super.adicionar(funcionario);
+    }
+
+    @Override
+    public Supervisor buscar(String nome) {
+        return super.buscar(nome);
+    }
+
+    @Override
+    public void remover(String nome) {
+        super.remover(nome);
+    }
+
+    @Override
+    public void mostrarTodos() {
+        super.mostrarTodos();
+    }
+
+    @Override
+    public void atualizarDados(String nome, double novoSalario, String novoCargo) {
+        super.atualizarDados(nome, novoSalario, novoCargo);
+    }
+}
--- src/funcionariosBlackcoders/Main.java
+++ src/funcionariosBlackcoders/Main.java
@@ -9,20 +9,32 @@ public class Main {
 
         // Adicionando Coordenador, supervisor e analista de Desenvolvimento
         Coordenador funcionario3 = new Coordenador("Tulio", "Coordenador", 4500, "4164565689", "Desenvolvimento");
+        Supervisor funcionario4 = new Supervisor("Guilherme", "Supervisor", 3800, "4884565689", "Desenvolvimento");
 
-        // Criando árvores de armazenamento
+        // Criando árvores de armazenamento - Filtragem por cargo
         ArvoreCEO arvoreCEO = new ArvoreCEO();
         ArvoreCTO arvoreCTO = new ArvoreCTO();
         ArvoreCoordenador arvoreCoordenador = new ArvoreCoordenador();
+        ArvoreSupervisor arvoreSupervisor = new ArvoreSupervisor();
 
         // Adicionando executivos em suas respectivas árvores
         arvoreCEO.adicionar(funcionario);
         arvoreCTO.adicionar(funcionario2);
         arvoreCoordenador.adicionar(funcionario3);
+        arvoreSupervisor.adicionar(funcionario4);
 
         // Testando CRUD
         arvoreCEO.mostrarTodos();
         arvoreCTO.mostrarTodos();
         arvoreCoordenador.mostrarTodos();
+        arvoreSupervisor.mostrarTodos();
+
+        // Criando uma árvore para todos os funcionários cadastrados
+        ArvoreDeFuncionarios arvoreTotal = new ArvoreDeFuncionarios<>();
+        arvoreTotal.adicionar(funcionario);
+        arvoreTotal.adicionar(funcionario2);
+        arvoreTotal.adicionar(funcionario3);
+        arvoreTotal.adicionar(funcionario4);
+        arvoreTotal.mostrarTodos();
     }
 }
