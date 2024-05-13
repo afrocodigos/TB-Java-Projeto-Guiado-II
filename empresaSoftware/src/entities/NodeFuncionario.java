9b628e925114689a0c633b33129791347e5cc4ca
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-05-13 20:04:11 -0300

    :recycle: refactor: corrige classe NodeFuncionario

--- empresaSoftware/src/entities/NodeFuncionario.java
+++ empresaSoftware/src/entities/NodeFuncionario.java
@@ -1,12 +1,11 @@
 package entities;
 
-public class NodeFuncionario extends FuncionarioBase{
+public class NodeFuncionario{
     FuncionarioBase funcionario;
     NodeFuncionario leftChild;
     NodeFuncionario rightChild;
 
     public NodeFuncionario(FuncionarioBase funcionario){
-        super(funcionario.getNomeFuncionario(), funcionario.getCargoFuncionario(), funcionario.getSalarioFuncionario(), funcionario.getCpfFuncionario());
         this.funcionario = funcionario;
         leftChild = null;
         rightChild = null;
