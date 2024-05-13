e662bf9f0265a34d17751377beab22c9f7fcfe04
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-05-13 12:31:22 -0300

    :sparkles: feat: Adiciona a interface InterfaceFuncionario
    Interface contendo os métodos get and set de nome, cargo, salário e cpf

---
+++ empresaSoftware/src/entities/InterfaceFuncionario.java
@@ -0,0 +1,12 @@
+package entities;
+
+public interface InterfaceFuncionario {
+    String getNomeFuncionario();
+    void setNomeFuncionario(String nomeFuncionario);
+    String getCargoFuncionario();
+    void setCargoFuncionario(String cargoFuncionario);
+    double getSalarioFuncionario();
+    void setSalarioFuncionario(double salarioFuncionario);
+    String getCpfFuncionario();
+    void setCpfFuncionario(String cpfFuncionario);
+}
