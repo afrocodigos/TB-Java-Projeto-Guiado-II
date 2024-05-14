9a473f1d6886c49d7824eee7abcd9cfe537b21f9
Lucas Teles <lucasouzateles@gmail.com>
2024-05-13 23:30:58 -0300

    :tada: commit inicial, começando meu projeto

---
+++ src/funcionariosBlackMoneySoftware/model/ArvoreDeFuncionarios.java
@@ -0,0 +1,50 @@
+package funcionariosBlackMoneySoftware.model;
+
+import java.util.TreeSet;
+
+public class ArvoreDeFuncionarios<T extends FuncionarioBase> {
+
+    private TreeSet<T> funcionarios;
+
+    public ArvoreDeFuncionarios() {
+        funcionarios = new TreeSet<>((f1, f2 -> f1.getNome().compareTo(f2.getNome())));
+    }
+
+    public void adicionarFuncionario(T funcionario){
+        funcionarios.add(funcionario);
+        System.out.println("Funcionário adicionado com sucesso");
+    }
+
+    public T buscar(String nome){
+        for (T funcionarioBase : funcionarios) {
+            if (funcionarioBase.getNome().equals(nome)){
+                return funcionarioBase;
+            }
+
+        }
+        System.out.println("Funcionário não existe na nossa base de dados");
+        return null;
+    }
+
+    public void removerFuncionario(String nome){
+        T funcionarioParaRemover = buscar(nome);
+        if (funcionarioParaRemover != null){
+            funcionarios.remove(funcionarioParaRemover);
+            System.out.println("Funcionário " + nome + "removido com sucesso");
+        } else {
+            System.out.println(nome + "Não foi encontrando na nossa base de dados");
+        }
+    }
+
+    public void atualizarDados(String nome, double novoSalario, String novoCargo){
+        T funcionarioParaAtualizar = buscar(nome);
+        if (funcionarioParaAtualizar != null){
+            funcionarioParaAtualizar.setSalario(novoSalario);
+            funcionarioParaAtualizar.setCargo(novoCargo);
+            System.out.println("Dados atualizados com sucesso");
+        } else {
+            System.out.println(nome + "não encontrade na nossa base de dados");
+        }
+    }
+
+}
---
+++ src/funcionariosBlackMoneySoftware/model/FuncionarioInterface.java
@@ -0,0 +1,12 @@
+package funcionariosBlackMoneySoftware.model;
+
+public interface FuncionarioInterface {
+    String getNome();
+    void setNome(String nome);
+    String getCargo();
+    void setCargo(String cargo);
+    double getSalario();
+    void setSalario(double salario);
+    String getCpf();
+    void setCpf(String cpf);
+}
