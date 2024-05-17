9a9203d297bc5eee143d81ea401b135232d0ca6a
heloisebastos <heloisebastos28@hotmail.com>
2024-05-16 21:20:15 -0400

    :recycle: refactor:Adiciona o atributo Id

--- src/funcionariosStarlabs/view/Main.java
+++ src/funcionariosStarlabs/view/Main.java
@@ -1,17 +1,21 @@
 package funcionariosStarlabs.view;
 
-import funcionariosStarlabs.controller.CTO;
+import java.util.logging.Level;
+
+import funcionariosStarlabs.controller.CLevel;
 import funcionariosStarlabs.model.ArvoreDeFuncionarios;
 
 public class Main {
 
    public static void main(String[] args) {
       // instancia funcionario
-      CTO novoCTO = new CTO("Juliao", "CTO", 20000, "123654");
+      CLevel cLevel = new CLevel(123, "Juliao", "CLevel", 20000, "123654");
 
       // adiciona funcionario na arvore
       ArvoreDeFuncionarios arvore = new ArvoreDeFuncionarios<>();
-      arvore.adicionar(novoCTO);
+      arvore.adicionar(cLevel);
+
+      arvore.mostrarTodos();
 
    }
 }
