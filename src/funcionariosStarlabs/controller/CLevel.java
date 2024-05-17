712223eb644d951120297dac2b2a61af0c4d9c07
heloisebastos <heloisebastos28@hotmail.com>
2024-05-16 21:39:35 -0400

    :broom: cleanup : remove linha de código comentada

--- src/funcionariosStarlabs/controller/CLevel.java
+++ src/funcionariosStarlabs/controller/CLevel.java
@@ -7,22 +7,4 @@ public class CLevel extends FuncionarioBase {
         super(id, nome, cargo, salario, cpf);
     }
 
-    // public void criar() {
-    // System.out.println("CTO criado com sucesso");
-    // }
-
-    // public void ler() {
-    // System.out.println("Nome " + nome + ", Cargo: " + cargo + ", Salário: R$" +
-    // salario);
-    // }
-
-    // public void atualizarDados(double novoSalario) {
-    // this.salario = novoSalario;
-    // System.out.println("Dados do CTO atualizado com sucesso!");
-    // }
-
-    // public void remover() {
-    // System.out.println("CTO removido com sucesso");
-    // }
-
 }
