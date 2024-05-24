4c206b4199ee2f7905dda160770536cb402f395f
matheus-lima-silva <150077612+matheus-lima-silva@users.noreply.github.com>
2024-05-24 16:01:49 -0300

    Feat: Realiza operacoes CRUD de forma aleatoria.

--- src/rhIbalo/view/main.java
+++ src/rhIbalo/view/main.java
@@ -1,21 +1,56 @@
 package rhIbalo.view;
-import rhIbalo.controller.Cto;
-import rhIbalo.controller.GeradorFuncionario;
-import rhIbalo.model.treeCto;
-import java.util.ArrayList;
-import java.util.List;
-import rhIbalo.model.treesetFunc;
 
+import rhIbalo.controller.Cto;
+import rhIbalo.controller.VPDeEng;
+import rhIbalo.model.treeCto;
+import rhIbalo.model.treeVp;
 import static rhIbalo.controller.GeradorFuncionario.gerarFuncionarioAleatorio;
 
 public class main {
+
     public static void main(String[] args) {
+        treeCto arvcto = new treeCto();
+        treeVp arvvp = new treeVp();
+        logicapreenchimento(arvcto, arvvp);
 
-
-        Cto funcionario = new Cto (gerarFuncionarioAleatorio().getNome(), gerarFuncionarioAleatorio().getCargo(), (gerarFuncionarioAleatorio().getSalario()), gerarFuncionarioAleatorio().getCpf());
-        treeCto arvcto  = new treeCto();
-        arvcto.adicionar(funcionario);
         arvcto.mostrarTodos();
+        arvvp.mostrarTodos();
+        arvvp.buscar("Laura");
+
+
+    }
+
+    private static void logicapreenchimento(treeCto arvcto, treeVp arvvp) {
+        Cto funcionario0 = new Cto("Miguel", "Desenvolvedor", 2800, "17163934777");
+        var i = 0;
+        arvcto.adicionar(funcionario0);
+        while (!funcionario0.getNome().equals(gerarFuncionarioAleatorio().getNome())) {
+            i++;
+            var funcionarioAl = gerarFuncionarioAleatorio();
+            if (i % 2 == 0) {
+                Cto funcionario = new Cto(funcionarioAl.getNome(), funcionarioAl.getCargo(), funcionarioAl.getSalario(), funcionarioAl.getCpf());
+                arvcto.adicionar(funcionario);
+                if ("Enzo".equals(funcionarioAl.getNome())) {
+                    funcionario.atualizarDados(0);
+                }
+            } else {
+                VPDeEng funcionario = new VPDeEng(funcionarioAl.getNome(), funcionarioAl.getCargo(), funcionarioAl.getSalario(), funcionarioAl.getCpf());
+                arvvp.adicionar(funcionario);
+                if ("Valentina".equals(funcionarioAl.getNome())) {
+                    funcionario.atualizarDados(0, "FariaLimer");
+                }
+            }
+
+        }
+
+        var funcionario1 = gerarFuncionarioAleatorio().getNome();
+        System.out.println(funcionario1);
+        if (!(arvcto.buscar(funcionario1) == null)){
+            arvcto.remover(funcionario1);
+        } else if (!(arvvp.buscar(funcionario1) == null)){
+            arvvp.remover(funcionario1);
+        }
 
     }
 }
+
